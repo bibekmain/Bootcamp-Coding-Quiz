1472d65a6c981a8abbf305a1938297a0650b584b
BibekM <bibekmbkb@gmail.com>
2023-06-13 12:37:12 -0700

    init for html and javascript file

---
+++ script.js
@@ -0,0 +1,5 @@
+var begin = document .querySelector("#begin");
+var timer = document.querySelector("#time");
+var highscore = document.querySelector("#highscore-btn");
+var quizEl = document.querySelector("#quiz");
+var questionNumber = document.querySelector("question-number");
